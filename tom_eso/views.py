7947a239dcb8601aebbdbf3733b6228871a6f3ee
William (Lindy) Lindstrom <llindstrom@lco.global>
2024-05-02 01:26:01 +0100

    return ChoiceField HTML with Folder choices for the ObservingRun

--- tom_eso/views.py
+++ tom_eso/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 
 from crispy_forms.templatetags.crispy_forms_filters import as_crispy_field
 
+from tom_eso.eso_api import ESOAPI
 from tom_eso.eso import ESOObservationForm
 
 logger = logging.getLogger(__name__)
@@ -24,3 +25,20 @@ def folders_for_observing_run(request):
     The ``observing_run_id`` of the selected ``p2_observing_run`` is passed as a URL
     parameter and is thus available from the request.GET QueryDict.
     """
+    # extract the observing run id from the request.GET QueryDict
+    observing_run_id = int(request.GET['p2_observing_run'])
+    # logger.debug(f'observing_run_id: {observing_run_id}')
+
+    # Get the folder name choices for the selected observing run
+    # NOTE: this is a potentially slow API call
+    # TODO: considing caching the results
+    folder_name_choices = ESOAPI().folder_name_choices(observing_run_id)
+
+    # Update the choices for the p2_folder_name field
+    form = ESOObservationForm(request.GET)  # populate the form with the request data
+    field = form['p2_folder_name']
+    field.field.choices = folder_name_choices
+
+    # get the HTML for the updated ChoiceField that will update the p2_folder_name in the DOM
+    field_html = as_crispy_field(form['p2_folder_name'])
+    return HttpResponse(field_html)
