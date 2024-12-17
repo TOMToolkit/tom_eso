3d86215449920cd0ccd00a4c106eba158ba19b18
William (Lindy) Lindstrom <llindstrom@lco.global>
2024-12-17 19:30:56 +0000

    add placeholder test

--- tom_eso/tests.py
+++ tom_eso/tests.py
@@ -1,3 +1,8 @@
-# from django.test import TestCase
+from django.test import TestCase
 
 # Create your tests here.
+
+
+class TestESO(TestCase):
+    def test_eso(self):
+        self.assertTrue(True)
