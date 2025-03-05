e7f541411532a2c2dff69a28cedba254b9691319
William (Lindy) Lindstrom <llindstrom@lco.global>
2025-03-05 19:54:14 +0000

    add User Profile for ESO Facility

--- tom_eso/models.py
+++ tom_eso/models.py
@@ -1,3 +1,10 @@
-# from django.db import models
+from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ESOProfile(models.Model):
+    """User Profile for ESO Facility"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} ESO Profile'
